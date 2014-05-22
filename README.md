114a5ba639bd762c9dfb9f591f4cc6d720ba26fd
Rafael Kassner <kassner@gmail.com>
2014-05-22 17:57:21 -0300

    Configurable CEP Webservice URL

--- README.md
+++ README.md
@@ -18,3 +18,8 @@ please remove from `app/design/frontend/base/default/layout/magefm_customer.xml`
 * This module make several changes to `base/default` phtml files, in order to
 include the necessary changes. If you in any way extends or modify those files,
 please take a look on project source before installing the module.
+
+Local CEP Database
+------------------
+
+If you want to host locally the CEP database to improve performance, check the [MageFM CEP Database](https://github.com/magefm/cep-database) project. You can configure the Base URL for this project in System > Configuration > MageFM > Customer > General > CEP Webservice.
--- app/code/community/MageFM/Customer/controllers/CepController.php
+++ app/code/community/MageFM/Customer/controllers/CepController.php
@@ -7,22 +7,22 @@ class MageFM_Customer_CepController extends Mage_Core_Controller_Front_Action
     {
         $this->getResponse()->setHeader('Content-Type', 'application/json');
         $result = array();
+        $webserviceUrl = (string) Mage::getStoreConfig('magefm_customer/general/cep_webservice');
 
         try {
+            if (empty($webserviceUrl)) {
+                throw new Exception(Mage::helper('magefm_customer')->__('CEP não encontrado. Preencha manualmente os dados.'));
+            }
+
             $cep = preg_replace('/([^0-9]*)/', '', $this->getRequest()->getParam('cep'));
 
             if (strlen($cep) != 8) {
                 throw new Exception(Mage::helper('magefm_customer')->__('CEP não encontrado. Preencha manualmente os dados.'));
             }
 
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_URL, 'http://brcep.com.br/?' . http_build_query(array(
-                'formato' => $this->getRequest()->getParam('formato', 'json'),
-                'cep' => $this->getRequest()->getParam('cep'),
+            $content = file_get_contents($webserviceUrl . '?' . http_build_query(array(
+                        'cep' => $this->getRequest()->getParam('cep'),
             )));
-
-            $content = curl_exec($ch);
 
             if (!$content) {
                 throw new Exception(Mage::helper('magefm_customer')->__('CEP não encontrado. Preencha manualmente os dados.'));
